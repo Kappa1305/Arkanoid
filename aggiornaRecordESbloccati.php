9e9f49f7bbff0ec7bc6a4327c72c473f5e6da5b2
lukappa <lukappa00@gmail.com>
2022-04-28 23:31:01 +0200

    aggiunta gestione dei record

--- aggiornaRecordESbloccati.php
+++ aggiornaRecordESbloccati.php
@@ -1,11 +1,36 @@
 <?php
 require_once "./Connection.php";
+
 if ($_POST) {
-    $livelloSbloccato = $_POST["livello"] + 1;
+    $livello = $_POST["livello"];
+    $punteggio = $_POST["punteggio"];
+    aggiornaSbloccati($livello, $connection);
+    aggiornaRecord($livello, $punteggio, $connection);
+}
+
+function aggiornaSbloccati($livello, $connection)
+{
+    $sql = "SELECT livello
+    FROM sbloccato
+        WHERE username ='" . $_SESSION["username"] . "';";
+    $result = mysqli_query($connection, $sql);
+    $i = 0;
+    while ($row = mysqli_fetch_assoc($result)) {
+        $sbloccati[$i++] = $row["livello"];
+    }
+    $livelloSbloccato = $livello + 1;
     if (!in_array($livelloSbloccato, $sbloccati)) {
         $sql = "INSERT INTO sbloccato VALUES 
-                ('" . $_SESSION["username"] . "', " . $livelloSbloccato . ", null)";
+            ('" . $_SESSION["username"] . "', " . $livelloSbloccato . ", null)";
         mysqli_query($connection, $sql);
     }
 }
-?>
+
+function aggiornaRecord($livello, $punteggio, $connection)
+{
+    $sql = "UPDATE sbloccato
+        SET highscore =".$punteggio."
+        WHERE username = '".$_SESSION["username"]."' and livello = ".$livello." and (highscore IS null OR highscore < ".$punteggio.");";
+    mysqli_query($connection, $sql);
+
+}
